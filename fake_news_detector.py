0b1e91c7e77b501b2829a4fa72357137b83f02eb
Evan Shoemaker <evan.shoemaker0@gmail.com>
2024-04-13 16:06:31 -0700

    convert true/false to 0/1

--- fake_news_detector.py
+++ fake_news_detector.py
@@ -1,3 +1,4 @@
+#https://medium.com/@skillcate/detecting-fake-news-with-a-bert-model-9c666e3cdd9b
 import polars as pl
 import numpy as np
 import pycaret
@@ -10,7 +11,6 @@ from sklearn.metrics import classification_report
 import torch
 import torch.nn as nn
 import opendatasets as od
-
 device = torch.device("cuda")
 dataset = 'https://www.kaggle.com/datasets/jainpooja/fake-news-detection'
 od.download(dataset)
@@ -22,6 +22,8 @@ true_news = true_news.with_columns(Target = pl.Series(['True']*len(true_news)))
 fake_news = fake_news.with_columns(Target = pl.Series(['False']*len(fake_news)))
 #Merge true and fake news into one dataset
 news = pl.concat([true_news,fake_news])
+#Change from true/false to 0/1
+news = news.with_columns(news['Target'].apply(lambda x: 1 if x == 'True' else 0).alias('label'))
 #Shuffle rows
 news = news.with_columns(random = pl.Series(np.random.rand(len(news))))
 shuffled_news = news.sort("random")
